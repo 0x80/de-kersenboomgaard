55005fb134f10c787054f13035c105d7ba91572e
Thijs Koerselman <thijskoerselman@gmail.com>
2025-06-23 11:02:17 +0200

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Navigation } from "~/components/navigation";
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Navigation />
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
