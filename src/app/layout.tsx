3f053c2e2dd2306b68d271cdcb1051c06adfa570
Thijs Koerselman <thijskoerselman@gmail.com>
2026-04-03 20:43:48 +0200

    Add speed insights and enable view transitions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Navigation } from "~/components/navigation";
@@ -85,6 +86,7 @@ export default function RootLayout({
         {children}
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
